ca6b573da1abdca5ccdd26a0d403c0b32e0f92fd
mutsumikodama <mutsumikodama+dev@gmail.com>
2015-05-23 01:19:26 +0000

    Make a basic User model (including secure passwords)

--- db/migrate/20150523003141_create_users.rb
+++ db/migrate/20150523003141_create_users.rb
@@ -1,9 +1,9 @@
-class UsersController < ApplicationController
+class CreateUsers < ActiveRecord::Migration
   def change
-    create_table user do |t|
+    create_table :users do |t|
       t.string :name
       t.string :email
-      
+
       t.timestamps
     end
   end
